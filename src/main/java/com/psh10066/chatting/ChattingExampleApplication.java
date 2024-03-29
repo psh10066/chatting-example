c4b18bcc1734ba6eb0adf3e32cd0a209e61a86df
이호준 <psh10066@gmail.com>
2024-03-29 22:29:59 +0900

    프로젝트 초기 생성

---
+++ src/main/java/com/psh10066/chatting/ChattingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.psh10066.chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psh10066/chatting/ChattingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psh10066.chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
